6a931817e563c8116a627760a4b33e5c65dbed7e
keroloussamy <keroloussamy98@gmail.com>
2022-05-07 04:24:49 +0200

    Add BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 // Put any other imports below so that CSS from your components takes precedence over default styles.
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
