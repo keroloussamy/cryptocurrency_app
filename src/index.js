90721a5158c1df74a698fcf8e92e55b0280a9eb4
keroloussamy <keroloussamy98@gmail.com>
2022-05-07 15:41:03 +0200

    Add store and provider

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import 'bootstrap/dist/css/bootstrap.css';
 // Put any other imports below so that CSS from your components takes precedence over default styles.
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+
+
 import App from './App';
+import store from './app/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
