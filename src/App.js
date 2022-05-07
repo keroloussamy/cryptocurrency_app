96065c3d3581682d8d270f82b9b30add75f78468
keroloussamy <keroloussamy98@gmail.com>
2022-05-07 04:26:10 +0200

    Add new way to import components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+import { Navbar } from './components'
+
 function App() {
   return (
-    <div className="fs-1">
-      Hello
+    <div className="full-container">
+      <div className='row m-0'>
+        <div className='col-2 p-0'>
+          <Navbar />
+        </div>
+        <div className='col-10'>
+          hhhhhhhhhhhhhhhhhhhhhhh
+        </div>
+      </div>
     </div>
   );
 }
