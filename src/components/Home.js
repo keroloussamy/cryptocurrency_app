0578cdcd2d327764fefbcbeadc02bef65fc7eab1
keroloussamy <keroloussamy98@gmail.com>
2022-05-07 22:57:07 +0200

    Add Show more btn to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,10 @@
 import React, { Fragment } from 'react'
 import millify from 'millify'
+import { Link } from 'react-router-dom';
+
 import { useGetCryptosQuery } from '../services/cryptoApi'
+import Cryptocurrencies from './Cryptocurrencies';
+import News from './News';
 
 const Home = () => {
   const { data, isFetching } = useGetCryptosQuery();
@@ -37,6 +41,19 @@ const Home = () => {
           <p>{millify(globalStats.totalMarkets)}</p>
         </div>
       </div>
+
+      <div className="d-flex justify-content-between align-items-baseline">
+        <h4>Top 10 Cryptos In The World</h4>
+        <Link to="/cryptocurrencies">Show more</Link>
+      </div>
+      <Cryptocurrencies simplified />
+
+      <div className="d-flex justify-content-between align-items-baseline">
+        <h4>Latest Crypto News</h4>
+        <Link to="/cryptocurrencies">Show more</Link>
+      </div>
+      <News simplified />
+
     </Fragment>
   )
 }
