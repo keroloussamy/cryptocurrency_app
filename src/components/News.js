9e9bbf51f54bdb7a87e113eba1e569a00b2d319e
keroloussamy <keroloussamy98@gmail.com>
2022-05-08 20:59:06 +0200

    Add fillter by category using Select

--- src/components/News.js
+++ src/components/News.js
@@ -1,17 +1,32 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import moment from 'moment';
 
 import { useGetCryptoNewsQuery } from '../services/cryptoNews';
+import { useGetCryptosQuery } from '../services/cryptoApi';
 
 const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
 
 const News = ({ simplified }) => {
-  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 6 : 12 });
+  const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
+  const { data } = useGetCryptosQuery(100);
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 });
 
   if (!cryptoNews?.value) return 'Loading ...';
-  console.log(cryptoNews)
+
   return (
     <Fragment>
+     {!simplified && (
+        <div className="d-flex mx-2 my-4">
+          <h3>Select by Category :</h3>
+          <select 
+            className="p-2 ms-auto"
+            placeholder="Select a Crypto"
+            onChange={(e) => setNewsCategory(e.target.value)}>
+            <option value="Cryptocurrency">Cryptocurrency</option>
+            {data?.data?.coins?.map((currency, i) => <option value={currency.name} key={i}>{currency.name}</option>)}
+          </select>
+        </div>
+      )}
       <div className="row">
       {cryptoNews.value.map((news, i) => (
         <div className="col-xl-4 col-sm-6 col-12" key={i}>
@@ -26,7 +41,7 @@ const News = ({ simplified }) => {
                 <p>{news.description.length > 100 ? `${news.description.substring(0, 100)}...` : news.description}</p>
               </div>
               <div className="d-flex justify-content-between">
-                <div className="d-flex">
+                <div className="d-flex gap-1">
                   <img className="rounded-circle" width="30" src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt="" />
                   <p className="my-auto">{news.provider[0]?.name}</p>
                 </div>
