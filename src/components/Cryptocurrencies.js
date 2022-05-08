a481122297b33978cb69d168633a0a494f08e709
keroloussamy <keroloussamy98@gmail.com>
2022-05-08 02:49:57 +0200

    Add Cryptocurrencies component

--- src/components/Cryptocurrencies.js
+++ src/components/Cryptocurrencies.js
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
+import millify from 'millify';
+import { Link } from 'react-router-dom';
 
-const Cryptocurrencies = () => {
+import { useGetCryptosQuery } from '../services/cryptoApi';
+
+
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count); //Get something called data and you rename it to cryptosList
+  const [cryptos, setCryptos] = useState();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    setCryptos(cryptosList?.data?.coins);
+
+    const filteredData = cryptosList?.data?.coins.filter((item) => item.name.toLowerCase().includes(searchTerm));
+
+    setCryptos(filteredData);
+  }, [cryptosList, searchTerm]); //execute this function when any of arrays values get changed. And it also executed at the begining.
+
   return (
-    <div>Cryptocurrencies</div>
+    <Fragment>
+      {!simplified && (
+        <div className="search-crypto">
+          <input
+            placeholder="Search Cryptocurrency"
+            onChange={(e) => setSearchTerm(e.target.value.toLowerCase())}
+          />
+        </div>
+      )}
+      <div className="row">
+      {cryptos?.map((currency) => (
+        <div className="col-xl-3 col-sm-6 col-12" key={currency.uuid}>
+          <Link key={currency.uuid} to={`/crypto/${currency.uuid}`} className="text-decoration-none text-black">
+            <div className="card p-3 m-2">
+              <div className="d-flex justify-content-between">
+                <h5 className="card-title">{`${currency.rank}. ${currency.name}`}</h5>
+                <img className="rounded-circle" width="30" src={currency.iconUrl} alt={currency.name} />
+              </div>
+              <hr />
+              <div className="card-body">
+                <p>Price: {millify(currency.price)}</p>
+                <p>Market Cap: {millify(currency.marketCap)}</p>
+                <p>Daily Change: {currency.change}%</p>
+              </div>
+            </div>
+          </Link>
+        </div>
+      ))}
+      </div>
+    </Fragment>
   )
 }
 
