6f0eb8fb32bb507a8b5375b24941d044f8523389
keroloussamy <keroloussamy98@gmail.com>
2022-05-11 13:14:47 +0200

    Update some details

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,13 @@ import { Link } from 'react-router-dom';
 import { useGetCryptosQuery } from '../services/cryptoApi'
 import Cryptocurrencies from './Cryptocurrencies';
 import News from './News';
+import Loader from './Loader';
 
 const Home = () => {
   const { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats;
 
-  if (isFetching) return 'Loading.....';
+  if (isFetching) return <Loader />;
 
   return (
     <Fragment>
@@ -50,7 +51,7 @@ const Home = () => {
 
       <div className="d-flex justify-content-between align-items-baseline mt-5 mb-2 px-2">
         <h3>Latest Crypto News</h3>
-        <Link to="/cryptocurrencies" className="btn btn-outline-primary">Show more</Link>
+        <Link to="/news" className="btn btn-outline-primary">Show more</Link>
       </div>
       <News simplified />
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,7 @@ const Navbar = () => {
   return (
     <div className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark min-vh-100 position-fixed">
       <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-        <svg className="bi me-2" width="40" height="32"></svg>
-        <span className="fs-4">Sidebar</span>
+        <span className="fs-4">Cryptocurrency</span>
       </a>
       <hr />
       <ul className="nav nav-pills flex-column mb-auto">
@@ -24,14 +23,6 @@ const Navbar = () => {
               <path fillRule="evenodd" d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A7.988 7.988 0 0 1 0 10zm8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3z" />
             </svg>
             Cryptocurrencies
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/exchanges" className="nav-link text-white">
-            <svg className="bi me-2" width="16" height="16">
-              <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 2h-4v3h4V4zm0 4h-4v3h4V8zm0 4h-4v3h3a1 1 0 0 0 1-1v-2zm-5 3v-3H6v3h4zm-5 0v-3H1v2a1 1 0 0 0 1 1h3zm-4-4h4V8H1v3zm0-4h4V4H1v3zm5-3v3h4V4H6zm4 4H6v3h4V8z" />
-            </svg>
-            Exchanges
           </NavLink>
         </li>
         <li>
